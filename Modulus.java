2479ce2644703364cca5dd5b8db389bf014f9165
21A91A04O5 <98264977+21A91A04O5@users.noreply.github.com>
2022-05-30 05:33:40 +0530

    import java.util.Scanner; class Modulus {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b,mod;         a=sc.nextInt();         b=sc.nextInt();         mod=a%b;         System.out.print(mod);     } }

---
+++ Modulus.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Modulus
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,mod;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        mod=a%b;
+        System.out.print(mod);
+    }
+}
