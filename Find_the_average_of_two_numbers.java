6a5ce228d783f5cceda6c885fb9c44555cf57d6c
21A91A04O5 <98264977+21A91A04O5@users.noreply.github.com>
2022-05-30 05:39:46 +0530

    import java.util.Scanner; class Avg {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b;         float c;         a=sc.nextInt();         b=sc.nextInt();         c=(float)(a+b)/2;         System.out.format("%.4f",c);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Avg
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        float c;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=(float)(a+b)/2;
+        System.out.format("%.4f",c);
+    }
+}
